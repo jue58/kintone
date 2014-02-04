9ae8dc272e08ebf8081f089a4f6af14caa17dc14
Rikiya Kawakami <rikiya-kawakami@nishimu.co.jp>
2014-02-04 14:37:36 +0900

    レコード1件取得/追加/更新の処理追加

--- lib/kintone/uri/record.rb
+++ lib/kintone/uri/record.rb
@@ -11,5 +11,13 @@ module Kintone::Uri
     def get(app, id)
       @api.get(PATH, {:app => app, :id => id})
     end
+
+    def create(app, record)
+      @api.post(PATH, {:app => app, :record => record})
+    end
+
+    def update(app, id, record)
+      @api.put(PATH, {:app => app, :id => id, :record => record})
+    end
   end
 end
