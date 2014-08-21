575a66912949bea8cd6273d2c892a96e39d1455c
Rikiya Kawakami <ricky.k.yang@gmail.com>
2014-08-21 20:48:52 +0900

    guestのテスト修正

--- spec/kintone/api/guest_spec.rb
+++ spec/kintone/api/guest_spec.rb
@@ -9,11 +9,91 @@ describe Kintone::Api::Guest do
 
   describe '#get_url' do
     subject { target.get_url(command) }
-    context '' do
-      let(:command) { 'path' }
 
-      it { expect(subject).to eq('/k/guest/1/v1/path.json') }
+    let(:command) { 'path' }
+
+    it { expect(subject).to eq('/k/guest/1/v1/path.json') }
+  end
+
+  describe '#get' do
+    before(:each) do
+      stub_request(
+        :get,
+        'https://example.cybozu.com/k/guest/1/v1/path.json'
+      )
+        .with(
+          headers: { 'X-Cybozu-Authorization' => 'QWRtaW5pc3RyYXRvcjpjeWJvenU=' },
+          query: params
+        )
+        .to_return(body: "{\"abc\":\"def\"}", status: 200)
     end
+
+    subject { target.get(path, params) }
+
+    let(:path) { '/k/guest/1/v1/path.json' }
+    let(:params) { { p1: 'abc', p2: 'def' } }
+
+    it { expect(subject).to eq 'abc' => 'def' }
+  end
+
+  describe '#post' do
+    before(:each) do
+      stub_request(
+        :post,
+        'https://example.cybozu.com/k/guest/1/v1/path.json'
+      )
+        .with(
+          headers: { 'X-Cybozu-Authorization' => 'QWRtaW5pc3RyYXRvcjpjeWJvenU=' },
+          body: "{\"p1\":\"abc\",\"p2\":\"def\"}"
+        )
+        .to_return(body: "{\"abc\":\"def\"}", status: 200)
+    end
+
+    subject { target.post(path, body) }
+
+    let(:path) { '/k/guest/1/v1/path.json' }
+    let(:body) { { 'p1' => 'abc', 'p2' => 'def' } }
+
+    it { expect(subject).to eq 'abc' => 'def' }
+  end
+
+  describe '#put' do
+    before(:each) do
+      stub_request(
+        :put,
+        'https://example.cybozu.com/k/guest/1/v1/path.json'
+      )
+        .with(
+          headers: { 'X-Cybozu-Authorization' => 'QWRtaW5pc3RyYXRvcjpjeWJvenU=' },
+          body: "{\"p1\":\"abc\",\"p2\":\"def\"}"
+        )
+        .to_return(body: "{\"abc\":\"def\"}", status: 200)
+    end
+
+    subject { target.put(path, body) }
+
+    let(:path) { '/k/guest/1/v1/path.json' }
+    let(:body) { { 'p1' => 'abc', 'p2' => 'def' } }
+
+    it { expect(subject).to eq 'abc' => 'def' }
+  end
+
+  describe '#delete' do
+    before(:each) do
+      stub_request(
+        :delete,
+        'https://example.cybozu.com/k/guest/1/v1/path.json?p1=abc&p2=def'
+      )
+        .with(headers: { 'X-Cybozu-Authorization' => 'QWRtaW5pc3RyYXRvcjpjeWJvenU=' })
+        .to_return(body: "{\"abc\":\"def\"}", status: 200)
+    end
+
+    subject { target.delete(path, params) }
+
+    let(:path) { '/k/guest/1/v1/path.json' }
+    let(:params) { { 'p1' => 'abc', 'p2' => 'def' } }
+
+    it { expect(subject).to eq 'abc' => 'def' }
   end
 
   describe '#record' do
