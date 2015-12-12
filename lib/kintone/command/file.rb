44f8ab05ca05363e798fc68bf9e9ea0baeeddff8
t1h <t-hirano@mtj.biglobe.ne.jp>
2015-12-13 00:32:36 +0900

    getメソッド追加

--- lib/kintone/command/file.rb
+++ lib/kintone/command/file.rb
@@ -5,6 +5,10 @@ class Kintone::Command::File < Kintone::Command
     'file'
   end
 
+  def get(file_key)
+    @api.get(@url, fileKey: file_key)
+  end
+
   def register(app, file)
     @api.file_post(@url, file)
   end
