152ce862ee2598bd9fe7110a77c050ad71266c73
sincrare <sincrare@sonicgarden.jp>
2020-03-25 18:11:20 +0900

    テストを追加

--- spec/kintone/kintone_error_spec.rb
+++ spec/kintone/kintone_error_spec.rb
@@ -89,5 +89,55 @@ describe Kintone::KintoneError do
         it { is_expected.to eq '必須です。' }
       end
     end
+
+    context 'bulk requestでValidation Errorが発生した場合' do
+      let(:messages) do
+        {
+          'results' =>
+            [
+              {
+                "code" => "CB_VA01",
+                "id" => "49ZYc7Nv0dbvJ7N4lVEu",
+                "message" => "入力内容が正しくありません。",
+                "errors" => {
+                  "record.取消"=> {
+                    "messages"=>["必須です。"]
+                  }
+                }
+              },
+              {}
+            ]
+          }
+      end
+
+      let(:http_status) { 400 }
+
+      it { is_expected.to eq '400 [CB_VA01] 入力内容が正しくありません。(49ZYc7Nv0dbvJ7N4lVEu)' }
+
+      describe '#message_text' do
+        subject { target.message_text }
+        it { is_expected.to eq '入力内容が正しくありません。' }
+      end
+
+      describe '#id' do
+        subject { target.id }
+        it { is_expected.to eq '49ZYc7Nv0dbvJ7N4lVEu' }
+      end
+
+      describe '#code' do
+        subject { target.code }
+        it { is_expected.to eq 'CB_VA01' }
+      end
+
+      describe '#http_status' do
+        subject { target.http_status }
+        it { is_expected.to eq 400 }
+      end
+
+      describe '#errors' do
+        subject { target.errors['record.取消']['messages'].first }
+        it { is_expected.to eq '必須です。' }
+      end
+    end
   end
 end
